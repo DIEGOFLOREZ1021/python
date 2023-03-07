e83b34ae3acb1e97df6fe2263a1d14bee6f0a001
Diego Florez <diegoflorez1021@gmail.com>
2023-03-07 23:53:37 +0100

    Ejercicios encuestas

---
+++ 01-Conceptos-Basicos/Ejercicios/05b-Encuesta.py
@@ -0,0 +1,16 @@
+#############################################################################
+#Requerimientos Funcionales
+#
+#      -> Visualizar la media de opinion 
+#      -> Visualizar el numero total de encuestas
+#      -> El resultado se muestra al escribir FIN
+#      -> El valor de opinion es entre 0 y 10
+#      -> Visualizar el promedio de edades Menor(-18), Medio(18-55), Mayor(+55)
+#############################################################################
+#   Utilizando listas de Python, 10 encuestados y valor entre 0-10
+#############################################################################
+""" toma de datos: input
+    listas vacias, añadiendo valores
+    while true, si escribe fin hacemos un break
+    total de encuestas con (len)
+"""
